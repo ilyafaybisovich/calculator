df8ed19e81f8809fcb825eb961ec7c5a959bdfc2
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-01-03 19:13:36 +0200

    Another modification to calculator file

--- calculator.rb
+++ calculator.rb
@@ -1,18 +1,38 @@
-#a great update for remote repo
+def is_not_a_number?(input)
+  input != input.to_i.to_s
+end
 
 def say(message)
   puts "=> #{message}"
 end
-say("What is the first number?")
-num1 = gets.chomp
+
+
+say ("Do you want to do a calculation? Please put Y for 'yes'.")
+answer = gets.chomp
+if answer == "Y"  
+begin
+  say("What is the first number?")
+num1 = gets.chomp 
+while is_not_a_number?(num1)
+  say("Please enter a number.")
+  num1 = gets.chomp
+end
+
 say("What is the second number?")
 num2 = gets.chomp
+while is_not_a_number?(num2)
+  say("Please enter a number.")
+  num2 = gets.chomp
+end
+
 say("Choose operation: 1) add 2) subtract 3) multiply 4) divide")
 operation = gets.chomp
+
 while operation != "1" && operation != "2" && operation != "3" && operation != "4" 
   say("Unrecognized operation. Please pick one of the 4 basic operations.")
   operation = gets.chomp
 end
+
 case operation
 when "1"
   result = num1.to_i+num2.to_i
@@ -27,4 +47,12 @@ when "4"
   end
   result = num1.to_f/num2.to_f
 end
+
 say("The result is #{result}")
+say("Do you want to do another calculation? Please put Y for 'yes'.")
+another = gets.chomp
+end while another == "Y"
+end
+say ("Thank you for using the calculator. Bye!")
+
+
